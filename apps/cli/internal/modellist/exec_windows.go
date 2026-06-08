fd8e9dfef3ab597cc59f8d19cf9914274f5025e7
ZheX <zhex81@gmail.com>
2026-06-09 11:50:11 +1200

    debug: log enriched PATH contents when computed, including hasOpenCodeBin check

--- apps/cli/internal/modellist/exec_windows.go
+++ apps/cli/internal/modellist/exec_windows.go
@@ -5,8 +5,10 @@ package modellist
 import (
 	"os"
 	"os/exec"
+	"strings"
 	"sync"
 
+	"github.com/rs/zerolog/log"
 	"yishan/apps/cli/internal/runtime/shellenv"
 )
 
@@ -18,6 +20,16 @@ var (
 func getEnrichedEnv() []string {
 	enrichedEnvOnce.Do(func() {
 		enrichedEnv = shellenv.ResolveEnvWithUserPath(os.Environ(), "")
+		pathVal := ""
+		for _, e := range enrichedEnv {
+			if strings.HasPrefix(e, "PATH=") {
+				pathVal = e[5:]
+				break
+			}
+		}
+		log.Info().
+			Int("pathLen", len(pathVal)).
+			Msg("enriched PATH computed")
 	})
 	return enrichedEnv
 }
